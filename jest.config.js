9ab15c94c1b7f326bc55fc99621579ba590e8c6a
akitak1290 <akitak1290@gmail.com>
2025-03-05 16:52:01 +0700

    feat: setup test runner and test assertions

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  moduleDirectories: ['node_modules', 'src'],
+};
